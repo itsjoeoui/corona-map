38b50c6c210bbaff4cd91b6902c1f7c38822b056
itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>
2021-01-21 00:14:01 -0500

    Get the beautiful slider going
    Signed-off-by: itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>

--- main.py
+++ main.py
@@ -4,41 +4,65 @@ import plotly.graph_objs as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
-df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
 
-data = dict(
-    type = 'choropleth',
-    locations = df['iso_code'],
-    locationmode = 'ISO-3',
-    colorscale = 'burgyl',
-    z = df[df['location'] != 'World']['total_cases'],
-    marker = dict(
-        line = dict(color = 'rgb(12,12,12)', width = 1)
-    ),
-    colorbar = dict(
-        title = 'Death Count (M)'
-    ),
-)
+# Create month and day columns
+df['month'] = df['date'].apply(lambda x: int(x.split('-')[1]))
+df['day'] = df['date'].apply(lambda x: int(x.split('-')[2]))
 
-layout = dict(
-    title = 'COVID-19 Data Visualization',
-    geo = dict(
-        showframe = True,
-        projection = {
-            'type':'equirectangular'
-        }
-    )
-)
-
-coronamap = go.Figure(data = [data], layout = layout)
+# Only grab December for now
+MONTH = 12
+df = df[(df['location'] != 'World') & (df['month'] == MONTH)]
 
 app = dash.Dash()
 
 app.layout = html.Div([
-    dcc.Graph(figure=coronamap)
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='day-slider',
+        min=df['day'].min(),
+        max=df['day'].max(),
+        value=df['day'].min(),
+        marks={str(day): str(day) for day in df['day'].unique()},
+        step=None
+    )
 ])
 
-app.run_server(debug=True, use_reloader=True) # Turn off reloader if inside Jupyter
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    Input('day-slider', 'value'))
+def update_figure(selected_day):
+    filtered_df = df[df['day'] == selected_day]
+    data = dict(
+        type = 'choropleth',
+        locations = filtered_df['iso_code'],
+        locationmode = 'ISO-3',
+        colorscale = 'burgyl',
+        z = filtered_df['total_cases'],
+        marker = dict(
+            line = dict(color = 'rgb(12,12,12)', width = 1)
+        ),
+        colorbar = dict(
+            title = 'Death Count (M)'
+        ),
+    )
 
-# iplot(coronamap)
+    layout = dict(
+        title = 'COVID-19 Data Visualization',
+        geo = dict(
+            showframe = True,
+            projection = {
+                'type':'equirectangular'
+            }
+        )
+    )
+
+    coronamap = go.Figure(data = [data], layout = layout)
+
+    coronamap.update_layout(transition_duration=500)
+
+    return coronamap
+
+app.run_server(debug=True, use_reloader=True) # Turn off reloader if inside Jupyter
